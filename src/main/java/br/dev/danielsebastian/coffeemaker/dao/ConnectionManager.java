4387ea7c70d8846fc0a20340ef8c01ee038cca96
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-05-19 19:54:26 -0300

    feat: add ConnectionManager to DB

---
+++ src/main/java/br/dev/danielsebastian/coffeemaker/dao/ConnectionManager.java
@@ -0,0 +1,38 @@
+package br.dev.danielsebastian.coffeemaker.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+    private static ConnectionManager connectionManager;
+
+    private ConnectionManager() {
+    }
+
+    public static ConnectionManager getInstance() {
+        if (connectionManager == null) {
+            connectionManager = new ConnectionManager();
+        }
+        return connectionManager;
+    }
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            connection = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@localhost:1521:XE",
+                    "SYSTEM",
+                    "root");
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+}
