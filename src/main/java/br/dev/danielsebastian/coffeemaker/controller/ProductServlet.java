e3bb8ec287593b54b0c232820beee3dbecaa550a
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-05-21 09:41:48 -0300

    feat: update controller for use category and create openFormRegister

--- src/main/java/br/dev/danielsebastian/coffeemaker/controller/ProductServlet.java
+++ src/main/java/br/dev/danielsebastian/coffeemaker/controller/ProductServlet.java
@@ -1,8 +1,10 @@
 package br.dev.danielsebastian.coffeemaker.controller;
 
+import br.dev.danielsebastian.coffeemaker.dao.CategoryDao;
 import br.dev.danielsebastian.coffeemaker.dao.ProductDao;
 import br.dev.danielsebastian.coffeemaker.exception.DBException;
 import br.dev.danielsebastian.coffeemaker.factory.DaoFactory;
+import br.dev.danielsebastian.coffeemaker.model.Category;
 import br.dev.danielsebastian.coffeemaker.model.Product;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
@@ -18,11 +20,13 @@ import java.util.List;
 @WebServlet("/product")
 public class ProductServlet extends HttpServlet {
 
-    private ProductDao dao;
+    private ProductDao productDao;
+    private CategoryDao categoryDao;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-        dao = DaoFactory.getProductDao();
+        productDao = DaoFactory.getProductDao();
+        categoryDao = DaoFactory.getCategoryDao();
     }
 
     @Override
@@ -47,18 +51,21 @@ public class ProductServlet extends HttpServlet {
         double value = Double.parseDouble(req.getParameter("value"));
         int quantity = Integer.parseInt(req.getParameter("quantity"));
         LocalDate dateCreated = LocalDate.parse(req.getParameter("dateCreated"));
+        Long idCategory = Long.parseLong(req.getParameter("category"));
 
         Product product = new Product(null, name, value, quantity, dateCreated);
+        product.setCategory(new Category(idCategory, null));
 
         try {
-            dao.register(product);
+            productDao.register(product);
             req.setAttribute("message", "product cadastrado com sucesso");
         } catch (DBException e) {
             e.printStackTrace();
             req.setAttribute("error", e.getMessage());
         }
         System.out.println("register");
-        req.getRequestDispatcher("register-product.jsp").forward(req, resp);
+        //req.getRequestDispatcher("register-product.jsp").forward(req, resp);
+        this.openFormRegister(req, resp);
     }
 
     private void updateProduct(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -67,11 +74,13 @@ public class ProductServlet extends HttpServlet {
         double value = Double.parseDouble(req.getParameter("value"));
         int quantity = Integer.parseInt(req.getParameter("quantity"));
         LocalDate dateCreated = LocalDate.parse(req.getParameter("dateCreated"));
+        Long idCategory = Long.parseLong(req.getParameter("category"));
 
         Product product = new Product(id, name, value, quantity, dateCreated);
+        product.setCategory(new Category(idCategory, null));
 
         try {
-            dao.update(product);
+            productDao.update(product);
             req.setAttribute("message", "product atualizado com sucesso");
         } catch (DBException e) {
             e.printStackTrace();
@@ -85,7 +94,7 @@ public class ProductServlet extends HttpServlet {
         Long id = Long.parseLong(req.getParameter("idDelete"));
 
         try {
-            dao.delete(id);
+            productDao.delete(id);
             req.setAttribute("message", "product deletado com sucesso");
         } catch (DBException e) {
             req.setAttribute("error", "Erro ao deletar o produto");
@@ -107,22 +116,34 @@ public class ProductServlet extends HttpServlet {
             case "open-edition-form":
                 openFormEdit(req, resp);
                 break;
-            default:
+            case "open-form-register":
+                this.openFormRegister(req, resp);
                 break;
         }
     }
 
     private void list(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("list");
-        List<Product> all = dao.findAll();
+        List<Product> all = productDao.findAll();
         req.setAttribute("products", all);
         req.getRequestDispatcher("list-product.jsp").forward(req, resp);
     }
 
     private void openFormEdit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Long id = Long.parseLong(req.getParameter("id"));
-        Product byId = dao.findById(id);
+        Product byId = productDao.findById(id);
+        this.loadCategories(req);
         req.setAttribute("product", byId);
         req.getRequestDispatcher("edit-product.jsp").forward(req, resp);
+    }
+
+    private void openFormRegister(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.loadCategories(req);
+        req.getRequestDispatcher("register-product.jsp").forward(req, resp);
+    }
+
+    private void loadCategories(HttpServletRequest req) {
+        List<Category> categories = categoryDao.findAll();
+        req.setAttribute("categories", categories);
     }
 }
